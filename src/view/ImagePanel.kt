bff544426b21c183181342e0c43475c6e477db46
SergeySkoret <serzh_skaredov@mail.ru>
2017-05-09 01:50:45 +0300

    ImagePanel. Extend jpanel, override method paintComponent

---
+++ src/view/ImagePanel.kt
@@ -0,0 +1,21 @@
+package view
+
+import javax.swing.JPanel
+import java.awt.Graphics
+import java.awt.image.BufferedImage
+
+/**
+ * Created by Sergey Skaredov.
+ * SPSU. 2017.
+ */
+
+class ImagePanel(val image: BufferedImage): JPanel() {
+
+    override fun paintComponent(g: Graphics) {
+        super.paintComponent(g)
+
+        g.drawImage(image, 0, 0, this)
+
+    }
+
+}
