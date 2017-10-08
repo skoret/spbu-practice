7ebd3eee7c3130c12dd0f4d431b34cbdd49c7e4b
SergeySkoret <serzh_skaredov@mail.ru>
2017-10-08 22:57:20 +0300

    tasks with maybe and tree

--- hometask/week3.hs
+++ hometask/week3.hs
@@ -1,11 +1,30 @@
 
 import Data.Maybe
 
-f1 :: (Maybe a -> b) -> Maybe a -> Maybe a -> Maybe a -> Maybe (b, b, b)
-f1 f a b c | isNothing a || isNothing b || isNothing c = Nothing
+t1 :: (Maybe a -> b) -> Maybe a -> Maybe a -> Maybe a -> Maybe (b, b, b)
+t1 f a b c | isNothing a || isNothing b || isNothing c = Nothing
            | otherwise = Just (f a, f b, f c)
 
-f1' :: (Maybe a -> b) -> [Maybe a] -> Maybe [b]
-f1' _ [] = Just []
-f1' f a | any isNothing a = Nothing
+t1' :: (Maybe a -> b) -> [Maybe a] -> Maybe [b]
+t1' _ [] = Just []
+t1' f a | any isNothing a = Nothing
         | otherwise = Just (map f a)
+
+data Tree a = Leaf | Node a (Tree a) (Tree a) deriving (Show, Eq)
+
+find Leaf _ = False
+find (Node a x y) b | a == b = True
+                    | otherwise = find x b || find y b
+
+mapT :: (a -> b) -> (Tree a) -> (Tree b)
+mapT f Leaf = Leaf
+mapT f (Node a x y) = Node (f a) (mapT f x) (mapT f y)
+
+data Tree' a = Leaf' | Node' a (Tree' a) (Tree' a) deriving (Show, Eq)
+
+find' Leaf' _ = False
+find' (Node' a x y) b | a == b = True
+                      | a > b = find' x b
+                      | otherwise = find' y b
+
+
