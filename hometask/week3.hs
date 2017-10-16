5582cfb414ecb43d46dceddd235beef9af9335ea
SergeySkoret <serzh_skaredov@mail.ru>
2017-10-16 16:39:53 +0300

    functions for type Maybe and Zipper

--- hometask/week3.hs
+++ hometask/week3.hs
@@ -1,14 +1,25 @@
 
 import Data.Maybe
 
-t1 :: (Maybe a -> b) -> Maybe a -> Maybe a -> Maybe a -> Maybe (b, b, b)
-t1 f a b c | isNothing a || isNothing b || isNothing c = Nothing
-           | otherwise = Just (f a, f b, f c)
+t1 :: (a -> b -> c -> d) -> Maybe a -> Maybe b -> Maybe c -> Maybe d
+t1 f a b c =
+        c >>= \x ->
+        b >>= \y ->
+        a >>= \z -> 
+        return (f z y x)
+ 
+t1' :: ([a] -> b) -> [Maybe a] -> Maybe b
+t1' f a = map (\x -> )
 
-t1' :: (Maybe a -> b) -> [Maybe a] -> Maybe [b]
-t1' _ [] = Just []
-t1' f a | any isNothing a = Nothing
-        | otherwise = Just (map f a)
+data Zipper a = Elem [a] a [a] deriving Show
+
+current (Elem _ b _) = b 
+
+next (Just (Elem a b (c:cs))) = Just (Elem (b:a) c cs)
+next _ = Nothing
+
+prev (Just (Elem (a:as) b c)) = Just (Elem as a (b:c))
+prev _ = Nothing
 
 data Tree a = Leaf | Node a (Tree a) (Tree a) deriving (Show, Eq)
 
