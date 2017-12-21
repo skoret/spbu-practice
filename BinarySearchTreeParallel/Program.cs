f89a0bba65a9a8530c2401df6041784cfcded008
SergeySkoret <serzh_skaredov@mail.ru>
2017-12-21 04:11:07 +0300

    parallel implementation of binary search tree with optimistic synchronization

---
+++ BinarySearchTreeParallel/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace BinarySearchTreeParallel
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var treeParallel = new TreeParallel<int, int>();
+            var tasks = new List<Task>();
+            const string help = "choose action:\n" +
+                                  "1 - insert pairs <key value> // for stop enter ' . '\n" +
+                                  "2 - find values by keys\n" +
+                                  "3 - delete values by keys\n" +
+                                  "4 - print tree // and perfom all operations\n" +
+                                  "5 - compare simple bst and parallel bst\n" +
+                                  "help - show cmds\n" +
+                                  "exit - end work with tree";
+            
+            Console.WriteLine(help);
+            
+            while (true)
+            {
+                Console.Write("> ");
+                var choice = Console.ReadLine();
+                        
+                switch (choice)
+                {
+                    case "1":
+
+                        var str = Console.ReadLine();
+                            
+                        while (str != null && str != ".")
+                        {
+                            var tmp = str.Split();
+
+                            if (int.TryParse(tmp[0], out var keyInsert)
+                                && int.TryParse(tmp[1], out var valueInsert))
+                            {
+                                tasks.Add(new Task(() =>
+                                {
+                                    Console.WriteLine("insert " + keyInsert);
+                                    treeParallel.Insert(keyInsert, valueInsert);
+                                }));
+                            }
+                            else
+                            {
+                                Console.WriteLine("invalid input, try again");
+                            }
+
+                            str = Console.ReadLine();
+                        }
+
+                        break;
+
+                    case "2":
+
+                        str = Console.ReadLine();
+                        
+                        while (str != ".")
+                        {
+                            if (int.TryParse(str, out var keySearch))
+                            {
+                                tasks.Add(new Task(() =>
+                                {
+                                    var node = treeParallel.Search(keySearch);
+                                    Console.WriteLine(node != null ? "node found: " + node : "doesn't found");
+                                }));
+                            }
+                            else
+                            {
+                                Console.WriteLine("invalid input, try again");
+                            }
+                            
+                            str = Console.ReadLine();
+                        }
+                        
+                        break;
+
+                    case "3":
+
+                        str = Console.ReadLine();
+                        
+                        while (str != null && str != ".")
+                        {
+                            if (int.TryParse(str, out var keyDelete))
+                            {
+                                tasks.Add(new Task(() => { 
+                                    Console.WriteLine("delete " + keyDelete);
+                                    treeParallel.Delete(keyDelete); 
+                                }));
+                            }
+                            else
+                            {
+                                Console.WriteLine("invalid input, try again");
+                            }
+                            
+                            str = Console.ReadLine();
+                        }
+                        
+                        break;
+
+                    case "4":
+                        foreach (var task in tasks)
+                        {
+                            task.Start();
+                        }
+                        Task.WaitAll(tasks.ToArray());
+                        tasks.Clear();
+                        treeParallel.Print();
+                        break;
+                        
+                    case "5":
+                        var rn = new Random();
+                        var sw = new Stopwatch();
+                        var tree0 = new Tree<int, int>();
+                        var tree1 = new TreeParallel<int, int>();
+                        var tree2 = new TreeParallel<int, int>();
+                        var tree3 = new TreeParallelAsync<int, int>();
+
+                        str = Console.ReadLine();
+                        if (str == null || !int.TryParse(str, out var amount))
+                        {
+                            Console.WriteLine("invalid input, try again");
+                            break;
+                        }
+                        
+                        Console.WriteLine("compare insertion on {0} keys:", amount);
+
+                        var keys = new int[amount];
+                        for (var i = 0; i < amount; i++)
+                        {
+                            keys[i] = rn.Next(3*amount);
+                        }
+                        
+                        sw.Start();
+                        foreach (var i in keys)
+                        {
+                            tree0.Insert(i, i);
+                        }
+                        sw.Stop();
+                        Console.WriteLine("seq insertion time in simple bst: {0}", sw.Elapsed);
+                        
+                        sw.Start();
+                        foreach (var i in keys)
+                        {
+                            tree1.Insert(i, i);
+                        }
+                        sw.Stop();
+                        Console.WriteLine("seq insertion time in locked bst: {0}", sw.Elapsed);
+
+                        var tasksList = keys.Select(arg => new Task(() => { tree2.Insert(arg, arg); })).ToList();
+                        sw.Restart();
+                        foreach (var task in tasksList)
+                        {
+                            task.Start();   
+                        }
+                        Task.WaitAll(tasksList.ToArray());
+                        sw.Stop();
+                        tasksList.Clear();
+                        Console.WriteLine("conc insertion time in locked bst: {0}", sw.Elapsed);
+                        
+                        tasksList = keys.Select(arg => new Task(async () => { await tree3.Insert(arg, arg); })).ToList();
+                        sw.Restart();
+                        foreach (var task in tasksList)
+                        {
+                            task.Start();   
+                        }
+                        Task.WaitAll(tasksList.ToArray());
+                        sw.Stop();
+                        tasksList.Clear();
+                        Console.WriteLine("conc insertion time in async locked bst: {0}", sw.Elapsed);
+                        
+                        Console.WriteLine("compare deletion:");
+
+                        sw.Start();
+                        for (var i = 0; i < amount; i += 3)
+                        {
+                            tree0.Delete(keys[i]);
+                        }
+                        sw.Stop();
+                        Console.WriteLine("seq deletion time in simple bst: {0}", sw.Elapsed);
+                        
+                        sw.Start();
+                        for (var i = 0; i < amount; i += 3)
+                        {
+                            tree1.Delete(keys[i]);
+                        }
+                        sw.Stop();
+                        Console.WriteLine("seq deletion time in locked bst: {0}", sw.Elapsed);
+
+                        for (var i = 0; i < amount; i += 3)
+                        {
+                            var arg = i;
+                            tasksList.Add(new Task(() => { tree2.Delete(keys[arg]); }));
+                        }
+                        
+                        sw.Restart();
+                        foreach (var task in tasksList)
+                        {
+                            task.Start();   
+                        }
+                        Task.WaitAll(tasksList.ToArray());
+                        sw.Stop();
+                        tasksList.Clear();
+                        Console.WriteLine("conc deletion time in locked bst: {0}", sw.Elapsed);
+                        
+                        for (var i = 0; i < amount; i += 3)
+                        {
+                            var arg = i;
+                            tasksList.Add(new Task(async () => { await tree3.Delete(keys[arg]); }));
+                        }
+                        
+                        sw.Restart();
+                        foreach (var task in tasksList)
+                        {
+                            task.Start();   
+                        }
+                        Task.WaitAll(tasksList.ToArray());
+                        sw.Stop();
+                        tasksList.Clear();
+                        Console.WriteLine("conc deletion time in async locked bst: {0}", sw.Elapsed);
+                        
+                        
+                        
+                        // tree1.Print();
+                        // tree2.Print();
+                        // tree3.Print();
+                        
+                        break;
+                        
+                    case "help":
+                        Console.WriteLine(help);
+                        break;
+                        
+                    case null:
+                    case "exit":
+                        Console.WriteLine("uhadi");
+                        return;
+                        
+                    default:
+                        Console.WriteLine("invalid input, try again");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ BinarySearchTreeParallel/TreeParallelAsync.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeParallel
+{
+    public class TreeParallelAsync<K, V> where K : IComparable<K>
+    {
+        private Node<K, V> _root;
+        private readonly object _locker = new object();
+        
+        private bool IsEmpty()
+        {
+            return _root == null;
+        }
+
+        public async Task<Node<K, V>> Search(K key)
+        {
+            return await Task.Run(() =>
+            {
+                var curr = _root;
+                while (curr != null && !curr.key.Equals(key))
+                {
+                    curr = curr.key.CompareTo(key) < 0 ? curr.right : curr.left;
+                }
+                return curr;
+            });
+        }
+
+        public async Task Insert(K key, V value)
+        {
+            await Task.Run(() =>
+            {
+                lock (_locker)
+                {
+                    if (_root == null)
+                    {
+                        _root = new Node<K, V>(key, value);
+                        return;
+                    }
+                }
+
+                while (true)
+                {
+                    var pair = Find(key);
+                    var curr = pair.Item1;
+                    var parent = pair.Item2;
+
+                    if (curr == null)
+                    {
+                        lock (parent)
+                        {
+                            var tmp = Find(key).Item2;
+                        
+                            if (tmp != parent) continue;
+                            
+                            curr = new Node<K, V>(key, value) {parent = parent};
+
+                            if (parent.right == null && parent.key.CompareTo(key) < 0)
+                            {
+                                parent.right = curr;
+                                return;
+                            }
+                            if (parent.left == null && parent.key.CompareTo(key) > 0)
+                            {
+                                parent.left = curr;
+                                return;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        lock (curr)
+                        {
+                            curr.value = value;
+                            return;
+                        }
+                    }
+                }
+            });
+        }
+        
+        public async Task Delete(K key)
+        {
+            await Task.Run(() =>
+            {
+               while (true)
+                {
+                    var pair = Find(key);
+                    var victim = pair.Item1;
+                    var parent = pair.Item2;
+                
+                    if (victim == null) 
+                        return;
+                    
+                    if (parent == null && victim.IsLeaf())
+                    {
+                        lock (victim)
+                        {
+                            pair = Find(key);
+                            var tmp = pair.Item1;
+                            var tmpParent = pair.Item2;
+                            
+                            if (tmp == null || tmpParent != null || !tmp.IsLeaf()) 
+                                continue;
+                            
+                            _root = null;
+                            return;
+                        }
+                    }
+                    else if (parent != null && victim.IsLeaf())
+                    {
+                        lock (parent)
+                        lock (victim)
+                        {
+                            pair = Find(key);
+                            var tmp = pair.Item1;
+                            var tmpParent = pair.Item2;
+                            
+                            if (tmp == null || tmpParent == null || !tmp.IsLeaf()) 
+                                continue;
+                            
+                            if (victim == victim.parent.right)
+                            {
+                                victim.parent.right = null;
+                            }
+                            else if (victim == victim.parent.left)
+                            {
+                                victim.parent.left = null;
+                            }
+                            return;
+                        }
+                    }
+                    else if (victim.right != null && victim.left == null)
+                    {
+                        if (parent == null)
+                        {
+                            lock (victim)
+                            {
+                                pair = Find(key);
+                                var tmp = pair.Item1;
+                                var tmpParent = pair.Item2;
+                                
+                                if (tmp == null || tmpParent != null || tmp.right == null || tmp.left != null)
+                                    continue;
+    
+                                victim.right.parent = victim.parent;
+                                _root = victim.right;
+                            }
+                        }
+                        else
+                        {
+                            lock (parent)
+                            lock (victim)
+                            {
+                                pair = Find(key);
+                                var tmp = pair.Item1;
+                                var tmpParent = pair.Item2;
+                                
+                                if (tmp == null || tmpParent == null || tmp.right == null || tmp.left != null) 
+                                    continue;
+    
+                                victim.right.parent = victim.parent;
+                                if (victim == victim.parent.left)
+                                    victim.parent.left = victim.right;
+                                else
+                                    victim.parent.right = victim.right;
+                            }
+                        }
+                        return;
+                    }
+                    else if (victim.right == null && victim.left != null)
+                    {
+                        if (parent == null)
+                        {
+                            lock (victim)
+                            {
+                                pair = Find(key);
+                                var tmp = pair.Item1;
+                                var tmpParent = pair.Item2;
+                                
+                                if (tmp == null || tmpParent != null || tmp.left == null || tmp.right != null) 
+                                    continue;
+    
+                                victim.left.parent = victim.parent;
+                                _root = victim.left;
+                            }
+                        }
+                        else
+                        {
+                            lock (parent)
+                            lock (victim)
+                            {
+                                pair = Find(key);
+                                var tmp = pair.Item1;
+                                var tmpParent = pair.Item2;
+                                
+                                if (tmp == null || tmpParent == null || tmp.left == null || tmp.right != null) 
+                                    continue;
+    
+                                victim.left.parent = victim.parent;
+                                if (victim == victim.parent.left)
+                                    victim.parent.left = victim.left;
+                                else
+                                    victim.parent.right = victim.left;
+                            }
+                        }
+                        return;
+                    }
+                    else if (parent != null)
+                    {
+                        lock (parent)
+                        lock (victim)
+                        {
+                            pair = Find(key);
+                            var tmp = pair.Item1;
+                            var tmpParent = pair.Item2;
+                            
+                            if (tmp == null || tmpParent == null || tmp.left == null || tmp.right == null) 
+                                continue;
+                            
+                            var node = FindMin(victim.right);
+    
+                            if (node == null) continue;
+    
+                            lock (node)
+                            {
+                                var tmpNode = FindMin(tmp.right);
+                                
+                                if (tmpNode == null || tmpNode != node || node.left != null) 
+                                    continue;
+                                
+                                victim.key = node.key;
+                                victim.value = node.value;
+    
+                                if (victim == node.parent)
+                                {
+                                    victim.right = node.right;
+                                }
+                                else
+                                {
+                                    node.parent.left = node.right;
+                                }
+                                if (node.right != null) node.right.parent = node.parent;
+                            }
+                        }
+                        return;
+                    }
+                    else
+                    {
+                        lock (victim)
+                        {
+                            pair = Find(key);
+                            var tmp = pair.Item1;
+                            var tmpParent = pair.Item2;
+                            
+                            if (tmp == null || tmpParent != null || tmp.left == null || tmp.right == null)
+                                continue;
+                            
+                            var node = FindMin(victim.right);
+    
+                            if (node == null) continue;
+    
+                            lock (node)
+                            {
+                                var tmpNode = FindMin(tmp.right);
+                                
+                                if (tmpNode == null || tmpNode != node || node.left != null)
+                                    continue;
+    
+                                victim.key = node.key;
+                                victim.value = node.value;
+    
+                                if (victim == node.parent)
+                                {
+                                    victim.right = node.right;
+                                }
+                                else
+                                {
+                                    node.parent.left = node.right;
+                                }
+                                if (node.right != null) 
+                                    node.right.parent = node.parent;
+                            }
+                        }
+                        return;
+                    }
+                }
+            });
+        }
+        
+        private Tuple<Node<K, V>, Node<K, V>> Find(K key)
+        {
+            var tmp = _root;
+            var tmpParent = tmp.parent;
+
+            while (tmp != null && !tmp.key.Equals(key))
+            {
+                tmpParent = tmp;
+                tmp = tmp.key.CompareTo(key) < 0 ? tmp.right : tmp.left;
+            }
+            
+            return new Tuple<Node<K, V>, Node<K, V>>(tmp, tmpParent);
+        }
+
+        private Node<K, V> FindMin()
+        {
+            return FindMin(_root);
+        }
+        
+        private Node<K, V> FindMin(Node<K, V> node)
+        {
+            if (node == null) return null;
+            var minNode = node;
+            while (minNode.left != null)
+            {
+                minNode = minNode.left;
+            }
+            return minNode;
+        }
+
+        private Node<K, V> FindMax()
+        {
+            return FindMax(_root);
+        }
+
+        private Node<K, V> FindMax(Node<K, V> node)
+        {
+            if (node == null) return null;
+            var maxNode = node;
+            while (maxNode.right != null)
+            {
+                maxNode = maxNode.right;
+            }
+            return maxNode;
+        }
+
+        private Node<K, V> NextSmaller(Node<K, V> node)
+        {
+            if (node == null) return null;
+            
+            var smaller = node;
+    
+            if (smaller.left != null) return FindMax(smaller.left);
+
+            while (smaller != null && smaller == smaller.parent?.left)
+            {
+                smaller = smaller.parent;
+            }
+
+            return smaller?.parent;
+        }
+    
+        // reverse in-order iterator
+        public IEnumerator<Node<K, V>> GetEnumerator()
+        {
+            var node = FindMax();
+            var last = FindMin();
+
+            while (node != null && node.key.CompareTo(last.key) >= 0)
+            {
+                var next = node;
+                node = NextSmaller(node);
+                yield return next;
+            }
+        }
+    
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("tree is empty");
+                return;
+            }
+
+            Console.WriteLine();
+            
+            foreach (var node in this)
+            {
+                var indent = "";
+                var i = 0;
+
+                while (i < node.High())
+                {
+                    i++;
+                    indent += "  ";
+                }
+
+                Console.WriteLine(indent + node.Branch() + node);
+            }
+
+            Console.WriteLine("_____________________");
+        }
+    }
+}
