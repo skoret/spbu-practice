55c90426f03a0dae5544c461c99997610e96d69c
kakadu <kakadu.hafanana@gmail.com>
2018-01-23 14:30:42 +0300

    WIP

--- PMMain.hs
+++ PMMain.hs
@@ -13,15 +13,17 @@ cases = [ "_ -> 1"
         , "_ -> 1-2"
         , "C(x,y)-> if ((tag x)=(tag y)) then 1 else 2"
         , "P(x,y) -> if 6<x then (if 19<y then 42 else 19) else 34"
+        , "A(2,x) -> 42"
+        , "P _ -> 42"
         ]
 scrutinees = [ "A(1)", "1"]
 
 main = do
   print "playground is here"
-  -- mapM_ (\line -> print line >> print (PMParser.parseRhs  optsE    line))
-  --   rhs
-  -- mapM_ (\line -> print line >> print (PMParser.parsePatt optsP    line))
-  --   patts
+  mapM_ (\line -> print line >> print (PMParser.parseRhs  optsE    line))
+    rhs
+  mapM_ (\line -> print line >> print (PMParser.parsePatt optsP    line))
+    patts
   -- mapM_ (\line -> print line >> print (PMParser.parseCase optsP optsE line))
   --   cases
   -- mapM_ (\line -> print line >> print (PMParser.parseScrutinee optsE line))
