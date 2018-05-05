2760c2a6d34d5df8027a58e2dd0cafb19f5e0eb6
skoret <sergey.skoret@gmail.com>
2018-05-05 22:51:08 +0300

    change camera viewpoint

--- throw and watch/Assets/Scripts/CameraController.cs
+++ throw and watch/Assets/Scripts/CameraController.cs
@@ -7,17 +7,21 @@ public class CameraController : MonoBehaviour {
     [SerializeField]
     private GameObject target;
 
+    public float speed;
+
 	// Use this for initialization
 	void Start () {
-
-        transform.position = new Vector3(target.transform.position.x,
-            target.transform.position.y + 4, target.transform.position.z - 14);
-        transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position, Vector3.up);
 
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+
+        Vector3 relativePosition = target.transform.position - transform.position;
+
+        Quaternion rotation = Quaternion.LookRotation(relativePosition, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0.0f, 0.0f));
+
+    }
 }
